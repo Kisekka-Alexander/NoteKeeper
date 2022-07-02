092a00614d63d73d7559333371d0462e520292bb
Kisekka-Alexander <53447358+Kisekka-Alexander@users.noreply.github.com>
2022-07-03 02:08:03 +0300

    Update NoteListActivity.kt

--- app/src/main/java/com/example/notekeeper/NoteListActivity.kt
+++ app/src/main/java/com/example/notekeeper/NoteListActivity.kt
@@ -2,6 +2,8 @@ package com.example.notekeeper
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -31,6 +33,8 @@ class NoteListActivity : AppCompatActivity() {
             val activityIntent = Intent(this, MainActivity::class.java)
             startActivity(activityIntent)
         }
+        findViewById<ListView>(R.id.listNotes).adapter = ArrayAdapter<NoteInfo>(this,android.R.layout.simple_list_item_1,
+        DataManager.notes)
     }
 
     override fun onSupportNavigateUp(): Boolean {

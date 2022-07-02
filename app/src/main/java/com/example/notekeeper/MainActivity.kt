eba470e9b8918d9116b05e19512ad9ff14d3a0c7
Kisekka-Alexander <53447358+Kisekka-Alexander@users.noreply.github.com>
2022-07-03 02:51:09 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/notekeeper/MainActivity.kt
+++ app/src/main/java/com/example/notekeeper/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.ArrayAdapter
+import android.widget.EditText
 import android.widget.Spinner
 import android.widget.TextView
 import com.example.notekeeper.databinding.ActivityMainBinding
@@ -18,6 +19,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private var notePosition = POSITION_NOT_SET
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,12 +38,22 @@ class MainActivity : AppCompatActivity() {
 //        dm.courses.values.toList()
           DataManager.courses.values.toList()
         )
-
         adapterCourses.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-
         findViewById<Spinner>(R.id.spinnerCourses).adapter = adapterCourses
 
+        notePosition = intent.getIntExtra(EXTRA_NOTE_POSITION, POSITION_NOT_SET)
+        if(notePosition != POSITION_NOT_SET)
+            displayNote()
 
+    }
+
+    private fun displayNote() {
+        val note = DataManager.notes[notePosition]
+        findViewById<EditText>(R.id.textNoteTitle).setText(note.title)
+        findViewById<EditText>(R.id.textNoteText).setText(note.text)
+
+        val coursePosition = DataManager.courses.values.indexOf(note.course)
+        findViewById<Spinner>(R.id.spinnerCourses).setSelection(coursePosition)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {

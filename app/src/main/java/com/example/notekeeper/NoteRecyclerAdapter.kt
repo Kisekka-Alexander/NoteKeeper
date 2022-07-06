ab61054426be29e09a1f1b8a45fcbe7927228542
Kisekka-Alexander <kkalexander717@gmail.com>
2022-07-07 01:45:55 +0300

    commit all

--- app/src/main/java/com/example/notekeeper/NoteRecyclerAdapter.kt
+++ app/src/main/java/com/example/notekeeper/NoteRecyclerAdapter.kt
@@ -1,17 +1,25 @@
 package com.example.notekeeper
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class NoteRecyclerAdapter:
+class NoteRecyclerAdapter(private val context: Context):
     RecyclerView.Adapter<NoteRecyclerAdapter.ViewHolder>() {
+
+    private val layoutInflater = LayoutInflater.from(context)
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val textCourse = itemView?.findViewById<TextView?>(R.id.textCourse)
+        val textTitle = itemView?.findViewById<TextView?>(R.id.textTitle)
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        val itemView = layoutInflater.inflate(R.layout.item_note_list, parent,false )
+        return ViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
